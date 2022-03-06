8188eb4926eec69c41dc2484c76a641eb8c30b57
Xiuyuan Wang <xiuyuan7@illinois.edu>
2022-03-05 21:27:57 -0600

    complete LS operation

--- server/tcpserver.py
+++ server/tcpserver.py
@@ -13,6 +13,8 @@
 # Import any necessary libraries below
 import socket
 import sys
+import os
+import subprocess
 
 
 ############## Beginning of Part 1 ##############
@@ -95,9 +97,163 @@ def part1 ():
 
 ############## Beginning of Part 2 ##############
 
+def changeWD(dirname, conn):
+    if os.path.exists(dirname) and os.path.isdir(dirname):
+        try:
+            os.chdir(dirname)
+            conn.sendall(b'1')
+        except Exception:
+            conn.sendall(b'-1')
+    else:
+        conn.sendall(b'-2')
+
+
+def removeFiles(filename, conn):
+    if os.path.exists(filename) and os.path.isfile(filename):
+        conn.sendall(b'1')
+        confirmation = conn.recv(BUFFER).decode()
+        if confirmation == 'yes':
+            os.remove(filename)
+            conn.sendall(b'1')
+        elif confirmation == 'no':
+            conn.sendall( b'-1')
+    else:
+        conn.sendall(b'-1')
+
+def removeDir(dirname, conn):
+    if os.path.exists(dirname) and os.path.isdir(dirname):
+        if len(os.listdir(dirname)) > 0:
+            conn.sendall(b'-2')
+        else:
+            conn.sendall(b'1')
+            confirmation = conn.recv(BUFFER).decode()
+            if confirmation == 'yes':
+                os.rmdir(dirname)
+                conn.sendall(b'1')
+            elif confirmation == 'no':
+                conn.sendall(b'-1')
+    else:
+        conn.sendall(b'-1')
+
+def listFiles(conn):
+    conn.sendall(subprocess.check_output(['ls', '-l']))
+
+
+def makeDirectory(dirname, conn):
+    if os.path.exists(dirname) and os.path.isdir(dirname):
+        conn.sendall(b'-2')
+
+    try:
+        os.makedirs(dirname)
+        conn.sendall(b'1')
+    except subprocess.CalledProcessError:
+        conn.sendall(b'-1')
+
+
+def handleQ(conn):
+    os.chdir('/'.join(__file__.split('/')[:-1])) # change the working directory to initial state
+    conn.close()
+
+def handleDN(filename, conn):
+    if os.path.exists(filename) and os.path.isfile(filename):
+        response = str(os.path.getsize(filename))+' '+filename
+        conn.sendall(response.encode())
+        str(os.path.getsize(filename)).encode()
+        with open(filename,'rb') as f:
+            chunk = f.read(BUFFER)
+            while chunk:
+                print(chunk)
+                conn.sendall(chunk)
+                chunk = f.read(BUFFER)
+            conn.sendall(b'-1')
+    else:
+        conn.sendall(b'-1')
+
+
+def handleUP(filename, conn):
+    conn.sendall(filename.encode())
+    response = conn.recv(BUFFER).decode()
+    if response == '-1':
+        pass
+    else:
+        filesize = int(response)
+        size = 0
+        with open(filename, 'wb') as f:
+            while size < filesize:
+                l = conn.recv(BUFFER)
+                f.write(l)
+                size += BUFFER
+
+handler = {}
+handler['LS'] = listFiles
+handler['MKDIR'] = makeDirectory
+handler['RMDIR'] = removeDir
+handler['RM'] = removeFiles
+handler['CD'] = changeWD
+handler['QUIT'] = handleQ
+handler['DN'] = handleDN
+handler['UP'] = handleUP
+
 # main function for Part 2
 def part2 ():
-    print()
+    print("********** PART 2 **********")
+
+    # TODO: fill in the IP address of the host and the port number
+    HOST = '127.0.0.1'
+    PORT = int(sys.argv[1])
+    sin = (HOST, PORT)
+
+    # TODO: create a datagram socket for TCP
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    except socket.error as e:
+        print('Failed to create socket.')
+        sys.exit()
+
+    # TODO: Bind the socket to address
+    try:
+        s.bind(sin)
+    except socket.error as e:
+        print('Failed to bind socket.')
+        sys.exit()
+
+    # TODO: start listening
+    try:
+        s.listen()
+    except socket.error as e:
+        print('Failed to listen to client.')
+        sys.exit()
+
+    while True:
+        print(f'Waiting for connections on port {PORT}')
+
+        # TODO: accept the connection and record the address of the client socket
+        try:
+            conn, addr = s.accept()
+        except socket.error as e:
+            print('Failed to accept connection from client.')
+            sys.exit()
+
+        print('Connection established.')
+
+        while True:
+            # receive command from client
+            try:
+                message = conn.recv(BUFFER).decode()
+            except socket.error as e:
+                print('Failed to receive command from client.')
+                sys.exit()
+
+            params = message.split()
+            handlerlabel = params[0]
+            arguments = params[1:] + [conn]
+            if handlerlabel not in handler:
+                respond = b'command not found'
+            else:
+                handler[handlerlabel](*arguments)
+            if conn.fileno() == -1:
+                break
+
 
 
 ############## End of Part 2 ##############
